251ee393811f5b2d08112b48d4a484f775a087b3
Nicholas Barbier <nbarbier@Nicholass-Air.hsd1.ma.comcast.net>
2025-03-17 15:00:20 -0400

    fixed symlink and tried to fix the dev container to have the python path and the symlink in the dev container

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ uv venv amira-project-env --python=3.11
 source amira-project-env/bin/activate
 uv pip install -r requirements.txt
 
+ln -s $(pwd)/config app/config
+
 export PYTHONPATH="$(pwd):$PYTHONPATH"
 streamlit run app/reader.py
--- setup_reprocess_examples.sh
+++ setup_reprocess_examples.sh
@@ -4,6 +4,8 @@ uv venv amira-project-env --python=3.11
 source amira-project-env/bin/activate
 uv pip install -r requirements.txt
 
+ln -s $(pwd)/config app/config
+
 python src/skill_match.py 
 python src/intervention_points.py
 
